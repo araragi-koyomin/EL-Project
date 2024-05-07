5aef4a4f0126c2268714678b47cf776e0a51f37d
araraki koyomin <3099146950@qq.com>
2024-05-07 17:06:54 +0800

    normal commit

--- Project/app/src/main/java/com/example/elcontest/MainPage.kt
+++ Project/app/src/main/java/com/example/elcontest/MainPage.kt
@@ -1,6 +1,8 @@
 package com.example.elcontest
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,37 +12,36 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.elcontest.ui.theme.ELContestTheme
 
-class MainActivity : ComponentActivity() {
+class MainPage : ComponentActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationDrawer: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ELContestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.mainpage)
+
+        drawerLayout = findViewById(R.id.MainPage)
+        navigationDrawer = findViewById(R.id.NavigationDrawer)
+
+        // 设置侧边栏的宽度为屏幕的75%
+        val params = navigationDrawer.layoutParams as DrawerLayout.LayoutParams
+        params.width = (resources.displayMetrics.widthPixels * 0.75).toInt()
+        navigationDrawer.layoutParams = params
+
+        // 设置ToggleButton的点击监听器
+        val toggleButton: Button = findViewById(R.id.ToggleButton)
+        toggleButton.setOnClickListener {
+            // 根据抽屉的状态打开或关闭
+            if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+                drawerLayout.closeDrawer(GravityCompat.END)
+            } else {
+                drawerLayout.openDrawer(GravityCompat.END)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ELContestTheme {
-        Greeting("Android")
     }
 }
